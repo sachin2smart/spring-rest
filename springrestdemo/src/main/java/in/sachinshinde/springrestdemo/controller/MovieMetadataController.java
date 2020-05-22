2f84e5209785006e3635b6a56cf1fcbe68041c7f
Sachin Shinde <sachin2smart@gmail.com>
2020-05-22 20:05:38 +0530

    Adding tests for java8 features

--- springrestdemo/src/main/java/in/sachinshinde/springrestdemo/controller/MovieMetadataController.java
+++ springrestdemo/src/main/java/in/sachinshinde/springrestdemo/controller/MovieMetadataController.java
@@ -6,6 +6,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
@@ -17,32 +20,62 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieMetadataController {
 	
 	@GetMapping("/getMovieMetadata")
-	public void getMovieMetadata() {
+	public List<String> getMovieMetadata() {
 		try{
 			BufferedReader reader = Files.newBufferedReader(Paths.get("src/main/resources/static/movies_metadata.csv"));
 			CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader().withIgnoreHeaderCase().withTrim());
 			
 			List<Integer> Ids = new ArrayList<Integer>();
 			List<String> original_titles = new ArrayList<String>();
-			List<String> runtimes = new ArrayList<String>();
+//			List<String> runtimes = new ArrayList<String>();
 
 			for(CSVRecord csvRecord: csvParser) {				 
 				 Ids.add(Integer.parseInt(csvRecord.get(5)));
 				 original_titles.add(csvRecord.get(8));
-				 runtimes.add(csvRecord.get(16));
+//				 runtimes.add(csvRecord.get(16));
 			}
+			 /*
+			  * The forEach method is a default method defined in the Iterable interface. 
+			  * The FOREACH function performs a computation on every member of a set or sets and assembles the results into a set.
+			  * Collection classes which extends Iterable interface can use forEach loop to iterate elements.
+			  * 
+			  * Here we have used ArrayList & iterated over the elements using the Lambda Expression & 
+			  * method reference.
+			  */
+			
+			// Iterating by using the Lambda expression
+//			 Ids.forEach((s)->System.out.println(s));
+//			 runtimes.forEach(s->System.out.println(s));
 			 
-			 Ids.forEach((s)->System.out.println(s));
-			 runtimes.forEach(s->System.out.println(s));
-			 original_titles.forEach(System.out::println);
+			// Iterating using the method reference
+//			 original_titles.forEach(System.out::println);
 			 
+			// **  filter() : Returns a stream consisting of the elements of this stream that match the given predicate.
+				//			Parameters: predicate
+				//			Returns: the new stream
+			// get the Movie names starting with a letter "T"
+//			 original_titles = original_titles.stream().filter(s->s.startsWith("T")).collect(Collectors.toList());
+			 
+			// get the movie names having title size in 5...8 exclusive
+			 original_titles = original_titles.stream().filter(s->s.length()>5 && s.length()<8).collect(Collectors.toList());
+			
+			// ** map() : Returns a stream consisting of the results of applying the given function to the elements of this stream.
+				//			Parameters: mapper 
+				//			Returns: the new stream		
+			
+//			original_titles = original_titles.stream().map().collect(Collectors.toList());
+//			Stream.concat(Ids.stream(),original_titles.stream()).forEach(System.out::println);
 			 csvParser.close();
-
+			 
+			 return original_titles;
+			 
 		}catch(FileNotFoundException e) {
 			System.out.println("File not found Exception: "+ e);
 		}catch(Exception e) {
 			System.out.println("Exception occured : " + e);
 		}
+		
+		return null;
 	}
 	
 }
