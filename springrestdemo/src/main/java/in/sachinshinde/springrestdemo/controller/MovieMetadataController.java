a611956f267319dbee1afa54406d2f53a318969b
Sachiksha <Sachiksha@DESKTOP-AP2P903>
2020-05-27 17:27:23 +0530

    Added examples for the intermediate and terminal operation of stream

--- springrestdemo/src/main/java/in/sachinshinde/springrestdemo/controller/MovieMetadataController.java
+++ springrestdemo/src/main/java/in/sachinshinde/springrestdemo/controller/MovieMetadataController.java
@@ -5,16 +5,22 @@ import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collector;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.mockito.internal.matchers.NotNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class MovieMetadataController {
@@ -27,12 +33,14 @@ public class MovieMetadataController {
 			
 			List<Integer> Ids = new ArrayList<Integer>();
 			List<String> original_titles = new ArrayList<String>();
-//			List<String> runtimes = new ArrayList<String>();
-
+			List<String> runtimes = new ArrayList<String>();
+			List<String> production_companies = new ArrayList<String>();
+			
 			for(CSVRecord csvRecord: csvParser) {				 
 				 Ids.add(Integer.parseInt(csvRecord.get(5)));
 				 original_titles.add(csvRecord.get(8));
-//				 runtimes.add(csvRecord.get(16));
+				 runtimes.add(csvRecord.get(16));
+				 production_companies.add(csvRecord.get(12));
 			}
 			 /*
 			  * The forEach method is a default method defined in the Iterable interface. 
@@ -57,17 +65,83 @@ public class MovieMetadataController {
 //			 original_titles = original_titles.stream().filter(s->s.startsWith("T")).collect(Collectors.toList());
 			 
 			// get the movie names having title size in 5...8 exclusive
-			 original_titles = original_titles.stream().filter(s->s.length()>5 && s.length()<8).collect(Collectors.toList());
+//			 original_titles = original_titles.stream().filter(s->s.length()>5 && s.length()<8).collect(Collectors.toList());
 			
 			// ** map() : Returns a stream consisting of the results of applying the given function to the elements of this stream.
 				//			Parameters: mapper 
 				//			Returns: the new stream		
 			
-//			original_titles = original_titles.stream().map().collect(Collectors.toList());
+			// ***** map() test 1 : get the movies names in upper case
+			
+//			original_titles = original_titles
+//									.stream()
+//									.map(String::toUpperCase)
+//									.collect(Collectors.toList());
+
+			// ***** map() test 2 : Get the runtime if the runtime is more than 80.
+			//						Specify it as a mini-clip, if the runtime is less than 80
+
+//			runtimes = runtimes
+//							.stream()
+//							.map(
+//										s->
+//											{
+//												if(!s.isEmpty()) 
+//													if(Double.parseDouble(s)>80) 
+//														return s; 
+//													else 
+//														return "This is a mini clip"; 
+//												else 
+//													return "Runtime not specified";
+//												}
+//								)
+//							.collect(Collectors.toList());
+			
 //			Stream.concat(Ids.stream(),original_titles.stream()).forEach(System.out::println);
-			 csvParser.close();
+//			production_companies.forEach(System.out::println);
+			production_companies = production_companies
+											.stream()
+											.map(
+													s->
+														{
+															s = s.toString().replace('\'', '"');
+															try {
+															JSONParser parser = new JSONParser();
+															Optional<JSONObject> jObj = (Optional<JSONObject>) parser.parse(s);
+//															
+																if(jObj.isPresent()) {
+																	System.out.println(jObj);
+																}else {
+																	System.out.println("Not Present.");
+																}
+//																Optional.ofNullable(s.getJSONObject())
+															}catch(Exception e) {
+																System.out.println("Exception :"+ e);
+															}
+															
+															return s;
+														}
+												)
+											.collect(Collectors.toList());
+			csvParser.close();
+			
+			// Get the maximum runtime among the movies 
+			Optional<Double> max_runtimes = runtimes
+												.stream()
+												.map(
+															s->
+																{
+																		if(!s.isEmpty()) 
+																			return Double.parseDouble(s);
+																		else
+																			return -9999.0;
+																}
+													)
+												.max(Comparator.naturalOrder());
+			
+			System.out.println("Min Runtime: "+max_runtimes.get());
 			 
-			 return original_titles;
+			 return production_companies;
 			 
 		}catch(FileNotFoundException e) {
 			System.out.println("File not found Exception: "+ e);
